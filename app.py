24ffc52571668e8d55283e82030f815ea26e900b
Abdullah Awad <abdullah.alashafi@gmail.com>
2021-12-20 00:16:53 +0300

    complete venue & artist column class model

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
-
+#* DONE 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
@@ -32,12 +32,19 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String()))
+    address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean())
+    seeking_description = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    
+    def __repr__(self):
+      return f"{self.name} venue in {self.city} city"
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -46,14 +53,22 @@ class Artist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String()))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(500))
+    seeking_venue = db.Column(db.Boolean())
+    seeking_description = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+
+    def __repr__(self):
+      return f"{self.name} artist in {self.city} city"
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class 
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
